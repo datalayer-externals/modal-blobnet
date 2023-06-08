ac1ec4c3bf1a5441b92036649fba99d8afd1e870
Daniel Norberg <dano@modal.com>
2023-06-08 19:31:34 +0200

    fix `--version` flag (#89)
    Currently the `--version` output might be stale due to https://github.com/baoyachi/shadow-rs#note-on-caching and https://github.com/baoyachi/shadow-rs/issues/95.
    
    Use https://crates.io/crates/vergen to work around and regenerate the version info as needed by ensuring that the build script is re-run when necessary.

--- blobnet/build.rs
+++ blobnet/build.rs
@@ -1,3 +1,13 @@
-fn main() -> shadow_rs::SdResult<()> {
-    shadow_rs::new()
+use std::error::Error;
+
+use vergen::EmitBuilder;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // Instruct cargo to re-run this build script for new git commits
+    EmitBuilder::builder().all_build().all_git().emit()?;
+
+    // Generate version information for use in `blobnet --version`.
+    shadow_rs::new()?;
+
+    Ok(())
 }
